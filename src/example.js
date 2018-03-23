9899ec13f133597727a48894120767fbcee37216
Liu Liu <leoliu1221@gmail.com>
2018-03-23 04:30:12 -0700

    frontend test

--- src/example.js
+++ src/example.js
@@ -2,73 +2,60 @@ import $ from 'jquery';
 import 'fullcalendar';
 
 $(function() {
+
+
+  $.getJSON('http://localhost:3000/jobs/machines', function(machines){
+      var dataList = document.getElementById('machinelist');
+      machines.forEach(function(item) {
+        var option = document.createElement('option');
+
+        option.value = item;
+
+        dataList.appendChild(option);
+      });
+    });
+
+    $('#machinesearch').on('propertychange input', function (e) {
+    var valueChanged = false;
+
+    if (e.type=='propertychange') {
+        valueChanged = e.originalEvent.propertyName=='value';
+    } else {
+        valueChanged = true;
+    }
+    if (valueChanged) {
+        // E.g. one machine e.target.value = m1
+        //now change the calendar view to that machine
+        console.log(e.target.value)
+        $('#calendar').fullCalendar('option', 'events', 'http://localhost:3000/jobs/machine='+e.target.value);
+        $('#calendar').fullCalendar('render');
+        $('#calendar').fullCalendar('rerendevents');
+    }
+});
+
+
+
   var containerEl = $('#calendar');
 
   containerEl.fullCalendar({
+        customButtons: {
+      chooseMachine: {
+        text: 'ChooseMachine',
+        click: function() {
+          alert('Choose machine name');
+        }
+      }
+    },
     header: {
-      left: 'prev,next today',
+      left: 'prev,next today chooseMachine',
       center: 'title',
       right: 'month,agendaWeek,agendaDay,listWeek'
     },
+    themeSystem: 'standard',
     defaultDate: '2018-01-12',
     navLinks: true, // can click day/week names to navigate views
     editable: true,
     eventLimit: true, // allow "more" link when too many events
-    events: [
-      {
-        title: 'All Day Event',
-        start: '2018-01-01',
-      },
-      {
-        title: 'Long Event',
-        start: '2018-01-07',
-        end: '2018-01-10'
-      },
-      {
-        id: 999,
-        title: 'Repeating Event',
-        start: '2018-01-09T16:00:00'
-      },
-      {
-        id: 999,
-        title: 'Repeating Event',
-        start: '2018-01-16T16:00:00'
-      },
-      {
-        title: 'Conference',
-        start: '2018-01-11',
-        end: '2018-01-13'
-      },
-      {
-        title: 'Meeting',
-        start: '2018-01-12T10:30:00',
-        end: '2018-01-12T12:30:00'
-      },
-      {
-        title: 'Lunch',
-        start: '2018-01-12T12:00:00'
-      },
-      {
-        title: 'Meeting',
-        start: '2018-01-12T14:30:00'
-      },
-      {
-        title: 'Happy Hour',
-        start: '2018-01-12T17:30:00'
-      },
-      {
-        title: 'Dinner',
-        start: '2018-01-12T20:00:00'
-      },
-      {
-        title: 'Birthday Party',
-        start: '2018-01-13T07:00:00'
-      },
-      {
-        title: 'Click for Google',
-        url: 'http://google.com/',
-        start: '2018-01-28'
-      }
-    ]
+    events: 'http://localhost:3000/jobs/machine=m1'
   })
 });
